d60a962a40a87204c4a51a34eaa54f07a076a39f
Daniela <dd.jj.rr@me.com>
2020-05-16 07:18:51 -0500

    Cambios a la rama formulario-nuevo-proveedor

--- cheaper/src/App.js
+++ cheaper/src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "./App.css";
-
-// import {
-// 	BrowserRouter as Router,
-// 	Route,
-// 	Swith,
-// 	Redirect,
-// 	Link,
-// } from "react-router-dom";
-import ProductListHome from "./components/productListHome.js";
+import NewProviderForm from "./components/newProviderForm.js";
 
 function App() {
 	return (
 		<div className="App">
-			<ProductListHome />
+			<NewProviderForm />
 
 		</div>
 	);
