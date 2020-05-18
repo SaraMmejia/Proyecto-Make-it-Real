08a59898475a0624f689222bd9b08bbded63c08d
danijrey <56493697+danijrey@users.noreply.github.com>
2020-05-18 12:10:13 -0500

    Lista de productos dinámica - Home (#8)
    * Formulario de ingreso proveedor nuevo
    
    * Formulario de ingreso proveedor nuevo
    
    * Lista de productos dinámica que hace parte del Home
    
    * Cambios a la rama formulario-nuevo-proveedor
    
    * Lista de productos dinámica - Home
    
    * Listos cambios primer Pull Request
    
    * Lista de productos del Home actualizada con los cambios requeridos
    
    * Se corrigen errores y se inicializan los estados del formulario con los métodos handleChange y handleSubmit
    
    * Modificado los tags generales al archivo App.css
    
    * Se modificaron los tags de estilos al archivo general App.css
    
    Co-authored-by: Daniela <dd.jj.rr@me.com>

--- cheaper/src/App.js
+++ cheaper/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import './App.css';
+import ProductListHome from "./components/productListHome.js";
 import Home from "./Components/Home";
+import NewProviderFormCreate from "./pages/NewProviderFormCreate";
 
 function App() {
 	return (
 		<div className="App">
+			<ProductListHome />
 			<Home />
+			<NewProviderFormCreate />
 		</div>
 	);
 }
---
+++ cheaper/src/components/dummyList.js
@@ -0,0 +1,68 @@
+import abarrotes from '../assets/abarrotes.jpg';//
+import aseoHogar from '../assets/aseoHogar.jpg'; //
+import cuidadoPersonal from '../assets/cuidadoPersonal.jpg';
+import carnes from '../assets/carnes.jpg';//
+import hogar from '../assets/hogar.jpg'; //
+import lacteos from '../assets/lacteos.jpg'; //
+import panaderia from '../assets/panaderia.jpg'; //
+import verduras from '../assets/verduras.jpg'; //
+import belleza from '../assets/belleza.png'; //
+
+
+const productList = [{
+  id: 'a',
+  image: panaderia,
+  name: 'Panadería fresca',
+  description: 'Son grandes',
+},
+{
+  id: 'b',
+  image: lacteos,
+  name: 'Leche',
+  description: 'Es rica',
+},
+{
+  id: 'c',
+  image: verduras,
+  name: 'Verduras',
+  description: 'Son grandes',
+},
+{
+  id: 'd',
+  image: carnes,
+  name: 'Carnes',
+  description: 'Es rica',
+},
+{
+  id: 'e',
+  image: abarrotes,
+  name: 'Abarrotes',
+  description: 'Son grandes',
+},
+{
+  id: 'f',
+  image: cuidadoPersonal,
+  name: 'Cuidado Personal',
+  description: 'Es rica',
+},
+{
+  id: 'g',
+  image: belleza,
+  name: 'Belleza',
+  description: 'Es rica',
+},
+{
+  id: 'h',
+  image: hogar,
+  name: 'Hogar',
+  description: 'Es rica',
+},
+{
+  id: 'i',
+  image: aseoHogar,
+  name: 'Abarrotes',
+  description: 'Son grandes',
+}
+]
+
+export default productList;
