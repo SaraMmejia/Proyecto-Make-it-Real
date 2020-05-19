ffb1ff45b204aeefdd02f203fbb367439301c210
danijrey <56493697+danijrey@users.noreply.github.com>
2020-05-19 18:14:00 -0500

    Primera unión de código, Home-Slider-List, Formulario cliente nuevo y… (#11)
    * Primera unión de código, Home-Slider-List, Formulario cliente nuevo y Formulario proveedor nuevo
    
    * Realizados los cambios de syntaxis y componente NavBar
    
    * Cambios de NavBar

--- cheaper/src/App.js
+++ cheaper/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
-import Slider from './components/slider';
-import FormClient from "./components/formClient.js";
-import NewProviderFormCreate from "./components/newProviderForm.js";
-import ProductListHome from "./components/productListHome.js";
+import ClientForm from "./pages/pgFormClient.js";
+import NewProvider from "./pages/pgNewProviderFormCreate.js";
 import Home from "./components/Home.js";
+import NavBar from "./components/NavBar.js";
+import IngresoSatisfactorio from "./pages/pgHome.js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  Link
+} from 'react-router-dom';
 
 function App() {
-	return (
-		<div className="App">
-     <Slider/>
-     <FormClient />
-     <NewProviderFormCreate />
-		 <ProductListHome />
-		 <Home />  
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={IngresoSatisfactorio} />
+          <Route exact path="/NewProviderFormCreate" component={NewProvider} />
+          <Route exact path="/FormClient" component={ClientForm} />
+          <Route path="*" component={IngresoSatisfactorio} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 //En esta sesión del código sólo se está renderizando el encabezado del home, falta anexarle todo lo de las rutas.
---
+++ cheaper/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/NewProviderFormCreate">Formulario proveedor Nuevo</Link>
+    <Link to="/FormClient">Formulario Cliente</Link>
+  </nav>
+)
+
+export default NavBar;
