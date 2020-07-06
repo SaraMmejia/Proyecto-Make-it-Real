2c94ea6fb71d9d6b3866643ebf2439a1bfc1f271
danijrey <56493697+danijrey@users.noreply.github.com>
2020-07-06 11:02:26 -0500

    Edit product front (#36)
    * Encabezado del Home
    
    * Realización de los comabios sugeridos en el Pull Requests
    
    * Listo los cambios de la primera resivión
    
    * Ya se borró el archivo debug.log
    
    * Cambios
    
    * Actualización
    
    * Ya renderiza
    
    * En esta nueva Rama, subo la pagina inicial del registro
    
    * Actualización página de registro
    
    * Corrección de todos los comentarios
    
    * Actualización de estilos
    
    * Actialización de rutas y links
    
    * Vista de Login
    
    * Actualización de formulario con sus respectivos estilos
    
    * Actalización de formulario de proveedores con sus respectivos estilos
    
    * Actualización con las correcciones ya listaas
    
    * Actualizaciones en Dev
    
    * Dev update
    
    * Lista edición de productos
    
    * Listos Cambios
    
    * Listos Cambios No.2
    
    Co-authored-by: SaraMmejia <sarammejia.11@gmail.com>
    Co-authored-by: Sara <57576199+SaraMmejia@users.noreply.github.com>

--- cheaper/src/components/sideDrower-Toolbar/sideDrawerClient.js
+++ cheaper/src/components/sideDrower-Toolbar/sideDrawerClient.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import './SideDrawer.css';
+import { useHistory } from "react-router-dom";
 
 
 const SideDrawerClient = props => {
-
+    let history = useHistory();
     let drawerClasses = 'side-drawer';
     if (props.show) {
         drawerClasses = 'side-drawer open';
     }
+    function handleClick() {
+        localStorage.clear();
+        history.push('/');
+    }
+    
     return (
         <nav className={drawerClasses}>
             <ul>
@@ -16,6 +22,7 @@ const SideDrawerClient = props => {
                 <li><a href="/home">Mis lista de mercado</a></li>
                 <li><a href="/home">Inicio</a></li>
                 <li><a href="/home">Configuracion</a></li>
+                <li className='btn_logout' onClick={handleClick}>Salir</li>
                 <img src={logo} className="side-drawer-logo" alt="Logo"></img>
             </ul>
         </nav>
